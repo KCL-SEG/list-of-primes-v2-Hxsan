f9e0f848d253804650d5898c4c205d537c7584f2
Hasan <mohammed.6.ahmed@kcl.ac.uk>
2023-09-29 15:52:48 +0100

    Add solution to Primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,29 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
+
     list = []
+    index = 2
+    while len(list) < number_of_primes:
+        if prime_separator(index) == True:
+            list.append(index)
+        index += 1
+
     return list
+
+def prime_separator(value):
+    if (value == 2) or (value == 3) or (value == 5) or (value == 7):
+        return True
+    elif value % 2 == 0:
+        return False
+    elif value % 3 == 0:
+        return False
+    elif value % 5 == 0:
+        return False
+    elif value % 7 == 0:
+        return False
+    
+    else:
+        return True
